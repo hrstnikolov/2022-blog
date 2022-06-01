266857af3e36a86b47130a47ba4eb00b785d2f78
hrstnikolov <hrstnikolov@gmail.com>
2022-06-01 20:55:09 +0300

    added tests

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from msilib.schema import ListView
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model=Post
+    template_name: str = 'post_detail.html'
